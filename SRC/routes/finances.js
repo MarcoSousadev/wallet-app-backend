ee3ed25953d976025af84e3079221b2a66892c0f
Marco Sousa <marcotgousa@gmail.com>
2024-11-12 22:29:04 -0300

    Feat: delete finance item implemented

--- SRC/routes/finances.js
+++ SRC/routes/finances.js
@@ -9,14 +9,14 @@ router.post('/', async (req, res) => {
     const { email } = req.headers
     const { category_id, title, date, value } = req.body
 
-    if (email.lenght < 5 || !email.includes('@')) {
+    if (email.length < 5 || !email.includes('@')) {
       return res.status(400).json({ error: 'e-mail is invalid' })
     }
     if (!category_id) {
       return res.status(400).json({ error: 'Category Id is mandatory' })
     }
 
-    if (!title && title.lenght < 3) {
+    if (!title && title.length < 3) {
       return res.status(400).json({
         error: 'Title is mandatory and should have more than 3 characters'
       })
@@ -25,7 +25,7 @@ router.post('/', async (req, res) => {
       return res.status(400).json({ error: 'Value is mandatory' })
     }
 
-    if (!date || date.lenght === 10) {
+    if (!date || date.length !== 10) {
       return res
         .status(400)
         .json({ error: 'Date is mandatory and should be in format yyyy-mm-dd' })
@@ -51,9 +51,52 @@ router.post('/', async (req, res) => {
       return res.status(500).json({ error: 'Finance row not created' })
     }
 
-    return res.status(200).json()
+    return res.status(200).json(createResponse.rows[0])
   } catch (error) {
     return res.status(500).json(error)
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const { email } = req.headers
+
+    if (email.length < 5 || !email.includes('@')) {
+      return res.status(400).json({ error: 'e-mail is invalid' })
+    }
+    if (!id) {
+      return res.status(400).json({ error: 'Id is mandatory' })
+    }
+    const userQuery = await db.query(usersQueries.findByEmail(email))
+    if (!userQuery.rows[0]) {
+      return res.status(404).send({ error: 'User does exists' })
+    }
+
+    const findFinancesText = 'SELECT * FROM finances WHERE id=$1'
+    const findFinancesValue = [Number(id)]
+    const financeItemQuery = await db.query(findFinancesText, findFinancesValue)
+
+    if (!financeItemQuery.rows[0]) {
+      return res.status(400).json({ error: 'finance row not found' })
+    }
+
+    if (financeItemQuery.rows[0].user_id !== userQuery.rows[0].id) {
+      return res
+        .status(401)
+        .send({ error: 'Finance row does not belong to user' })
+    }
+
+    const text = 'DELETE FROM finances WHERE id=$1 RETURNING *'
+    const values = [Number(id)]
+    const deleteResponse = await db.query(text, values)
+
+    if (!deleteResponse.rows[0]) {
+      return res.status(400).json({ error: 'finance row not deleted' })
+    }
+    return res.status(200).json(deleteResponse.rows[0])
+  } catch (error) {
+    return res.status(500).json({ error })
   }
 })
 
